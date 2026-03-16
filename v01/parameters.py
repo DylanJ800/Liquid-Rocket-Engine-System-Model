d138ac48dabfcd1a95cb9e62c399c9c757b82022
DylanJ800 <dylansjoseph2@gmail.com>
2026-03-16 13:15:04 -0400

    added varible definitions

--- v01/parameters.py
+++ v01/parameters.py
@@ -13,3 +13,21 @@ class EngineParameters:
 
     # Injector Properties 
     cd_ox: float = 0.8
+    cd_fuel: float = 0
+    area_ox_m2: float = 0
+    area_fuel_m2: float = 0
+
+    # Fluid Properties 
+    rho_ox_kg_m3: float = 0
+    rho_fuel_kg_m3: float = 0
+
+
+    # Feed Pressures 
+    tank_pressure_Pa: float = 000
+    piston_pressure_loss_Pa: float = 0
+
+    # Timing 
+    t_open_ox_s: float = 0
+    t_open_fuel_s: float = 0
+    t_shutdown_s: float = 0
+
